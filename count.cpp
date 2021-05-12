3bf8061fe44f6103af565eee9cdd1c61a2e14841
jothsnatippana <65223493+jothsnatippana@users.noreply.github.com>
2021-05-12 17:15:07 +0530

    Update count.cpp

--- count.cpp
+++ count.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,r,m,i,c=0,d=0;
+int n,r,c=0,d=0;
 void count(int num)
 {
     while(num>0)
 	{
 		r=num%10;
 		num=num/10;
-	if(r%2==0)
-	{
-		c++;
-	}
-	if(r%2!=0)
-	{
-		d++;
-	}
-  }
+	        if(r%2==0)
+	        {
+		     c++;
+	        }
+	        if(r%2!=0)
+	        {
+		     d++;
+	        }
+        }
   cout<<"even count is "<<c<<endl;
   cout<<"odd count is "<<d<<endl;  
 		
