7a83587bf7132ab467cda435e3bdb3a53056285f
jothsnatippana <65223493+jothsnatippana@users.noreply.github.com>
2021-06-07 10:52:28 +0530

    Update factorial_recursion.cpp

--- factorial_recursion.cpp
+++ factorial_recursion.cpp
@@ -6,28 +6,24 @@ int fact(int n)
 	if(n==0||n==1)
 	  return dp[n]=1;
 	if(dp[n]!=-1)
-	{
 	   return dp[n];
-    }
-return dp[n]=n*fact(n-1); 
+   return dp[n]=n*fact(n-1); 
 }
 int main()
 {
-	int n;
-	cin>>n;
+    int n;
+    cin>>n;
 	for(int i=0;i<=n;i++)
 	    dp[i]=-1;
-	    while(n>=0)
-	    {
+	while(n>=0)
+	{
 	    	int m=n;
 	    	cout<<fact(n)<<endl;
 	    	cin>>n;
 	    	 if(n>m)
 	    	 {
 	    	 	for(int i=m+1;i<=n;i++)
-	    	 	{
 	    	 		dp[i]=-1;
-				 }
-			 }
-		}
+	         }
+       }
 }
