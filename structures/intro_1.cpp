b76179211fcd2bdde07aa183aa0306b935a86123
jothsnatippana <65223493+jothsnatippana@users.noreply.github.com>
2021-10-28 06:32:08 +0530

    Create intro_1.cpp

---
+++ structures/intro_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+struct student 
+{
+    int rollno;
+    string name;
+    int marks;
+    int age;
+}s1={34,"manju",75,19};
+int main()
+{
+  cout<<s1.rollno<<endl;
+  cout<<s1.name<<endl;
+  cout<<s1.marks<<endl;
+  cout<<s1.age<<endl;
+}
